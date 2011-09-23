5f09dd5f8c7033560fe0ae16c3bd959c34904fb7
Michael Hoffer <info@michaelhoffer.de>
2011-09-23 11:11:55 +0200

    uses new policy enabled configurator

--- VRL-VTK/src/edu/gcsc/vrl/vtk/Configurator.java
+++ VRL-VTK/src/edu/gcsc/vrl/vtk/Configurator.java
@@ -9,13 +9,20 @@ import eu.mihosoft.vrl.system.PluginConfigurator;
 import eu.mihosoft.vrl.system.PluginDependency;
 import eu.mihosoft.vrl.system.PluginIdentifier;
 import eu.mihosoft.vrl.system.VPluginAPI;
+import eu.mihosoft.vrl.system.VPluginConfigurator;
 import java.awt.image.BufferedImage;
 
 /**
  *
  * @author Michael Hoffer <info@michaelhoffer.de>
  */
-public class Configurator implements PluginConfigurator{
+public class Configurator extends VPluginConfigurator{
+
+    public Configurator() {
+        setIdentifier(new PluginIdentifier("VRL-VTK", "0.1"));
+        setDescription("Simple VTK Viewer");
+    }
+
 
     public void register(PluginAPI api) {
         VPluginAPI vapi = (VPluginAPI)api;
@@ -26,24 +33,8 @@ public class Configurator implements PluginConfigurator{
 //        throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public String getDescription() {
-        return "Simple VTK Viewer";
-    }
-
-    public BufferedImage getIcon() {
-        return null;
-    }
-
-    public PluginIdentifier getIdentifier() {
-        return new PluginIdentifier("VRL-VTK", "0.1");
-    }
-
     public void init() {
-        //
-    }
-
-    public PluginDependency[] getDependencies() {
-        return new PluginDependency[0];
+       //
     }
     
 }
